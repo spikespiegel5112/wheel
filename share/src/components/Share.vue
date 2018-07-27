bb93a56e922b371fba21f773c7d427ed686d1770
spikespiegel5112 <wbl@zan-qian.com>
2018-07-27 11:01:52 +0800

    try

--- share/src/components/Share.vue
+++ share/src/components/Share.vue
@@ -344,7 +344,7 @@
           console.log(response)
 
           wx.config(Object.assign({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx67c26ff8068af257', // 必填，公众号的唯一标识
             timestamp: response.data.timestamp, // 必填，生成签名的时间戳
             nonceStr: response.data.nonceStr, // 必填，生成签名的随机串
