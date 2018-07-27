525083135af24ae74e391bf405336027aab0fe2c
spikespiegel5112 <wbl@zan-qian.com>
2018-07-27 19:13:28 +0800

    try

--- share/src/components/Share.vue
+++ share/src/components/Share.vue
@@ -390,8 +390,7 @@
             wx.onMenuShareTimeline({
               title: '免费畅享全年NBA直播的机会在这里', // 分享标题
               link: 'http://activity.fnvalley.com/?routeto=shareredirect', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              // imgUrl: 'http://activity.fnvalley.com' + '/static/img/404.b92dcc1.png', // 分享图标
-              imgUrl: '/static/img/share/qiang.png', // 分享图标
+              imgUrl: 'http://activity.fnvalley.com' + '/static/img/redpacket1.png', // 分享图标
 
               success: function () {
 
@@ -402,12 +401,11 @@
               title: '免费畅享全年NBA直播的机会在这里', // 分享标题
               desc: '千万不要错过哦', // 分享描述
               link: 'http://activity.fnvalley.com/?routeto=shareredirect', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              // imgUrl: 'http://activity.fnvalley.com' + '/static/img/404.b92dcc1.png', // 分享图标
-              imgUrl: '/static/img/share/qiang.png', // 分享图标
+              imgUrl: 'http://activity.fnvalley.com' + '/static/img/redpacket1.png', // 分享图标
               type: '', // 分享类型,music、video或link，不填默认为link
               dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
               success: function () {
-                alert('ddd')
+                // alert('ddd')
 // 用户点击了分享后执行的回调函数
               }
             });
